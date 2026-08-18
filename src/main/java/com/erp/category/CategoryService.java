7c20478bf2cfeae55d25a30556ca943174db1c2e
Khushi A Choudhary <khushiiachaudary@gmail.com>
2026-08-18 23:58:32 +0530

    Initial backend setup with category module

---
+++ src/main/java/com/erp/category/CategoryService.java
@@ -0,0 +1,45 @@
+package com.erp.category;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id, Category updatedCategory) {
+
+        Category existingCategory = getCategoryById(id);
+
+        existingCategory.setName(updatedCategory.getName());
+        existingCategory.setDescription(updatedCategory.getDescription());
+
+        return categoryRepository.save(existingCategory);
+    }
+
+    public void deleteCategory(Long id) {
+
+        Category category = getCategoryById(id);
+
+        categoryRepository.delete(category);
+    }
+}
